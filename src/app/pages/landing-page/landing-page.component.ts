39a7b8f26e6cff7b032b92f6b1eba144715b7be6
R.G. <frontend-rob@proton.me>
2025-04-13 11:44:12 +0200

    feat(landing-page): add landing page
    update includes:
    - landing page layout,
    - smooth animations,

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-landing-page',
-    imports: [RouterLink],
+    imports: [
+        CommonModule,
+        RouterLink
+    ],
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.component.scss'
 })
 
 export class LandingPageComponent {
+
+    theme: string | undefined;
+
+    constructor() {
+        this.initializeTheme();
+    }
+
+    initializeTheme(): void {
+        this.theme = this.getSavedTheme() || this.getPreferredTheme();
+    }
+
+    getSavedTheme(): string | null {
+        return localStorage.getItem('theme');
+    }
+
+    getPreferredTheme(): string {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+
+    toggleTheme(): void {
+        const newTheme = this.theme === 'light' ? 'dark' : 'light';
+        document.documentElement.setAttribute('data-theme', newTheme);
+        this.updateThemeColor(newTheme);
+        this.theme = newTheme;
+        localStorage.setItem('theme', newTheme);
+    }
+
+    updateThemeColor(theme: string): void {
+        const metaThemeColor = document.querySelector('meta[name="theme-color"]');
+        const themeColor = theme === 'dark' ? '#1c1c1f' : '#ffffff';
+        metaThemeColor?.setAttribute('content', themeColor);
+    }
 
 }
--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from '../../shared/sidebar/sidebar.component';
 import { ToolbarComponent } from '../../shared/toolbar/toolbar.component';
 
 @Component({
     selector: 'app-privacy-policy',
     imports: [
-        SidebarComponent,
         ToolbarComponent
     ],
     templateUrl: './privacy-policy.component.html',
