651946cebf3eee835566d29059c7535d0c99e108
R.G. <frontend-rob@proton.me>
2025-04-16 18:38:17 +0200

    add legal notice and privacy policy

--- src/app/pages/legal-notice/legal-notice.component.ts
+++ src/app/pages/legal-notice/legal-notice.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from '../../shared/toolbar/toolbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-legal-notice',
     imports: [
+        RouterLink,
         ToolbarComponent
     ],
     templateUrl: './legal-notice.component.html',
     styleUrls: [
         './legal-notice.component.scss',
-        '../../shared/sidebar/sidebar.component.scss',
         '../../shared/toolbar/toolbar.component.scss',
+        '../../../styles/_article.scss',
     ]
 })
 
--- src/app/pages/privacy-policy/privacy-policy.component.ts
+++ src/app/pages/privacy-policy/privacy-policy.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { ToolbarComponent } from '../../shared/toolbar/toolbar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-privacy-policy',
     imports: [
+        RouterLink,
         ToolbarComponent
     ],
     templateUrl: './privacy-policy.component.html',
     styleUrls: [
         './privacy-policy.component.scss',
-        '../../shared/sidebar/sidebar.component.scss',
         '../../shared/toolbar/toolbar.component.scss',
+        '../../../styles/_article.scss',
     ]
 })
 
